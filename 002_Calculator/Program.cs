4563d6a96cb47993301b4d4ea684912a5258df71
Clemilton da Costa <37125419+Clemilton10@users.noreply.github.com>
2022-04-07 05:28:05 -0300

    inicio

---
+++ 002_Calculator/Program.cs
@@ -0,0 +1,124 @@
+﻿using System;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Menu();
+        }
+        static void Menu()
+        {
+            Console.Clear();
+            Console.WriteLine("Que operação deseja fazer?");
+            Console.WriteLine("1-Soma");
+            Console.WriteLine("2-Sutração");
+            Console.WriteLine("3-Divisão");
+            Console.WriteLine("4-Multiplicação");
+            Console.WriteLine("5-Sair");
+            Console.WriteLine("--------------------------");
+            Console.WriteLine("Selecione:");
+            Console.WriteLine("");
+            string vl = Console.ReadLine() ?? "5";
+            short r = short.Parse(vl);
+            switch(r)
+            {
+                case 1: Soma(); break;
+                case 2: Subtracao(); break;
+                case 3: Divisao(); break;
+                case 4: Multiplicacao(); break;
+                case 5: System.Environment.Exit(0); break;
+                default: Menu(); break;
+            }
+        }
+        static void Soma()
+        {
+            Console.Clear();
+            Console.WriteLine("Primeiro valor:");
+            string sv1 = Console.ReadLine() ?? "0";
+            float v1 = float.Parse(sv1);
+
+            Console.Clear();
+            Console.WriteLine(v1);
+            Console.WriteLine("Segundo valor:");
+            string sv2 = Console.ReadLine() ?? "0";
+            float v2 = float.Parse(sv2);
+
+            //float sm = v1 + v2;
+            Console.Clear();
+            //Console.WriteLine("O Resultado é " + sm);
+            //Console.WriteLine("O Resultado é " + (v1 + v2) );
+            //Console.WriteLine($"O Resultado é {sm}");
+            Console.WriteLine($"O Resultado é {v1 + v2}");
+            Console.ReadKey();
+            Menu();
+        }
+        static void Subtracao()
+        {
+            Console.Clear();
+            Console.WriteLine("Primeiro valor:");
+            string sv1 = Console.ReadLine() ?? "0";
+            float v1 = float.Parse(sv1);
+
+            Console.Clear();
+            Console.WriteLine(v1);
+            Console.WriteLine("Segundo valor:");
+            string sv2 = Console.ReadLine() ?? "0";
+            float v2 = float.Parse(sv2);
+
+            //float sm = v1 - v2;
+            Console.Clear();
+            //Console.WriteLine("O Resultado é " + sm);
+            //Console.WriteLine("O Resultado é " + (v1 - v2) );
+            //Console.WriteLine($"O Resultado é {sm}");
+            Console.WriteLine($"O Resultado é {v1 - v2}");
+            Console.ReadKey();
+            Menu();
+        }
+        static void Divisao()
+        {
+            Console.Clear();
+            Console.WriteLine("Primeiro valor:");
+            string sv1 = Console.ReadLine() ?? "0";
+            float v1 = float.Parse(sv1);
+
+            Console.Clear();
+            Console.WriteLine(v1);
+            Console.WriteLine("Segundo valor:");
+            string sv2 = Console.ReadLine() ?? "0";
+            float v2 = float.Parse(sv2);
+
+            //float sm = v1 / v2;
+            Console.Clear();
+            //Console.WriteLine("O Resultado é " + sm);
+            //Console.WriteLine("O Resultado é " + (v1 / v2) );
+            //Console.WriteLine($"O Resultado é {sm}");
+            Console.WriteLine($"O Resultado é {v1 / v2}");
+            Console.ReadKey();
+            Menu();
+        }
+        static void Multiplicacao()
+        {
+            Console.Clear();
+            Console.WriteLine("Primeiro valor:");
+            string sv1 = Console.ReadLine() ?? "0";
+            float v1 = float.Parse(sv1);
+
+            Console.Clear();
+            Console.WriteLine(v1);
+            Console.WriteLine("Segundo valor:");
+            string sv2 = Console.ReadLine() ?? "0";
+            float v2 = float.Parse(sv2);
+
+            //float sm = v1 * v2;
+            Console.Clear();
+            //Console.WriteLine("O Resultado é " + sm);
+            //Console.WriteLine("O Resultado é " + (v1 * v2) );
+            //Console.WriteLine($"O Resultado é {sm}");
+            Console.WriteLine($"O Resultado é {v1 * v2}");
+            Console.ReadKey();
+            Menu();
+        }
+    }
+}
---
+++ 003_Stopwatch/Program.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Threading;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Menu();
+        }
+        static void Menu()
+        {
+            try
+            {
+                Console.Clear();
+                Console.WriteLine("S-Segundos => 10s = 10 Segundos");
+                Console.WriteLine("M-Minutos => 1m = 1 Minuto");
+                Console.WriteLine("0-Sair");
+                Console.WriteLine("Quanto tempo deseja contar?");
+
+                string data = Console.ReadLine().ToLower();
+                char type = char.Parse( data.Substring( data.Length - 1, 1 ) );
+                int time = int.Parse( data.Substring(0, data.Length - 1) );
+                //Console.WriteLine(data);
+                //Console.WriteLine(type);
+                //Console.WriteLine(time);
+                Console.Clear();
+                Console.WriteLine("0-Não");
+                Console.WriteLine("1-Sim");
+                Console.WriteLine("A contagem é regressiva?");
+                int rg = int.Parse( Console.ReadLine() );
+
+                int multiplier = 1;
+
+                if(type == 'm') multiplier = 60;
+
+                if(data == "0")
+                    System.Environment.Exit(0);
+                
+                PreStart(time * multiplier, rg);
+            }catch(Exception er)
+            {
+                Console.Clear();
+                Console.WriteLine("Menu:");
+                Console.WriteLine(er.Message);
+            }
+        }
+        static void PreStart(int time, int rg)
+        {
+            try
+            {
+                Console.Clear();
+                Console.WriteLine("Ready...");
+                Thread.Sleep(1000);
+                Console.WriteLine("Set...");
+                Thread.Sleep(1000);
+                Console.WriteLine("Go...");
+                Thread.Sleep(1000);
+                Start(time, rg);
+            }catch(Exception er)
+            {
+                Console.Clear();
+                Console.WriteLine("PreStart:");
+                Console.WriteLine(er.Message);
+            }
+        }
+        static void Start(int time = 10, int rg = 0)
+        {
+            try
+            {
+                if(rg == 0)
+                {
+                    /*
+                    int currentTime = 0;
+                    while(currentTime != time)
+                    {
+                        currentTime++;
+                        Console.Clear();
+                        Console.WriteLine(currentTime);
+                        Thread.Sleep(1000);
+                    }
+                    */
+                    for(int i = 0; i < time; i++)
+                    {
+                        Console.Clear();
+                        Console.WriteLine(i);
+                        Thread.Sleep(1000);
+                    }
+                }else
+                {
+                    for(int i = time; i > 0; i--)
+                    {
+                        Console.Clear();
+                        Console.WriteLine(i);
+                        Thread.Sleep(1000);
+                    }
+                }
+            }catch(Exception er)
+            {
+                Console.Clear();
+                Console.WriteLine("Start:");
+                Console.WriteLine(er.Message);
+            }
+        }
+    }
+}
